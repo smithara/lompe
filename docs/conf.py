0602cf963eaa7a097d25065cd49939eb558efff7
Ashley Smith <ashley.smith@magneticearth.org>
2023-04-18 12:52:22 +0200

    Exploratory docs setup with myst_nb etc

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Lompe"
+copyright = "2023, The Lompe developers"
+author = "The Lompe developers"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "myst_nb",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+# https://www.sphinx-doc.org/en/master/usage/extensions/autosummary.html
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+    "inherited-members": True,
+}
+
+# https://myst-nb.readthedocs.io/en/latest/configuration.html
+nb_execution_mode = "off"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
